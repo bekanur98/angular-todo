3c87979b406aeb56834d1b0b9a639f0813c89bbf
bekanur98 <bekanur98@gmail.com>
2020-06-22 15:45:57 +0600

    generate new project todo and added first component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CategoriesComponent } from './views/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data/TestData.ts
@@ -0,0 +1,46 @@
+import {Category} from '../model/Category';
+import {Priority} from '../model/Priority';
+import {Task} from '../model/Task';
+
+export class TestData {
+
+  static categories: Category[] = [
+    {id: 1, title: 'Работа'},
+    {id: 2, title: 'Семья'},
+    {id: 3, title: 'Учеба'},
+    {id: 4, title: 'Отдых'},
+    {id: 5, title: 'Спорт'},
+    {id: 6, title: 'Еда'},
+    {id: 7, title: 'Финансы'},
+    {id: 8, title: 'Гаджеты'},
+    {id: 9, title: 'Здоровье'},
+    {id: 10, title: 'Автомобиль'},
+  ];
+
+  static priorities: Priority[] = [
+    {id: 1, title: 'Низкий', color: '#e5e5e5'},
+    {id: 2, title: 'Средний', color: '#85D1B2'},
+    {id: 3, title: 'Высокий', color: '#F1929D'},
+    {id: 4, title: 'Очень срочно!!', color: '#F1128D'},
+  ];
+
+  static tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Залить бензин полный бак',
+      priority: TestData.priorities[2],
+      completed: false,
+      category: TestData.categories[9],
+      date: new Date('2020-06-23')
+    },
+
+    {
+      id: 2,
+      title: 'Передать отчеты начальнику управления',
+      priority: TestData.priorities[0],
+      completed: false,
+      category: TestData.categories[0],
+      date: new Date('2020-06-27')
+    },
+  ];
+}
